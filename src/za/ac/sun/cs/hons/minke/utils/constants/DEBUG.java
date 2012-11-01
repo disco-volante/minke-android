7afc45c43b9f341977d12cbacc3c256596c6783f
pieter <p.j.g.jordaan@gmail.com>
2012-11-01 16:45:28 +0200

    Debug off

--- src/za/ac/sun/cs/hons/minke/utils/constants/DEBUG.java
+++ src/za/ac/sun/cs/hons/minke/utils/constants/DEBUG.java
@@ -3,6 +3,6 @@ package za.ac.sun.cs.hons.minke.utils.constants;
 public class DEBUG {
 	public static boolean SIGNED = true;
 	public static boolean ON = true;
-	public static boolean EMULATOR = true;
-	public static boolean LOCAL = true ;
+	public static boolean EMULATOR = false;
+	public static boolean LOCAL = false ;
 }
